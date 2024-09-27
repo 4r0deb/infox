ad771fbcd8183bd2b2ea2c383cfd6c92a0b9c3bf
DumitruLuta <157763464+DumitruLuta@users.noreply.github.com>
2024-09-27 18:23:18 +0300

    Create info.py

---
+++ info.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import sys
+import platform
+import os
+import subprocess
+
+def sys_info():
+    print("")
+    #print(platform.uname())
+    os.system("uname -amr;echo '';pwd")
+
+def help():
+    os.system("echo 'The following options are valid: '| pv -qL 10")
+    print("sys              --system")
+    print("list             --ls -lah")
+    print("man              --man")
+def list():
+    list_files = subprocess.run(["ls", "-l"])
+    print("The exit code was: %d" % list_files.returncode)
+
+def man():
+    os.system("x-terminal-emulator -b -noclose -e man x-terminal-emulator &")
+
+
+
+if __name__ == "__main__":
+    opt = sys.argv[1] if len(sys.argv) > 1 else None
+
+    if opt == "sys":
+        sys_info()
+    elif opt == "help":
+        help()
+    elif opt == "list":
+        list()
+    elif opt == "man":
+        man()
+    else:
+        print("      Please run info help")
+#        print("      bala         --BALA")
+#        print("      list        --list")
